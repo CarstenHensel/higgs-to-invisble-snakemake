c0a45356973e3eb6ae1703ca9605dd4b3c17a0be
CarstenHensel <hensel.carsten@gmail.com>
2025-10-11 15:49:01 +0200

    beautify the submit_all.sh script

--- scripts/generate_grid_jobs.py
+++ scripts/generate_grid_jobs.py
@@ -334,7 +334,7 @@ def write_master_submit(job_keys):
         lines.append(f"cd {genid}_{prodid}/")
         lines.append(f"python3 submit_grid_{genid}_{prodid}.py")
         lines.append(f"cd ../")
-        lines.append(f"sleep 10")
+        lines.append(f"sleep 10\n")
     with open(script_path, "w") as f:
         f.write("\n".join(lines))
     os.chmod(script_path, 0o755)
